43e7c9b57547318c1741865a1262539ada2c9125
P-Vries <43247243+P-Vries@users.noreply.github.com>
2018-10-09 12:34:39 +0200

    Add files via upload

---
+++ Ouderbijdrage/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Ouderbijdrage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnpnlOk = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblID = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.txbaantalKinderen = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblOut = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnpnlOk
+            // 
+            this.btnpnlOk.Location = new System.Drawing.Point(120, 72);
+            this.btnpnlOk.Name = "btnpnlOk";
+            this.btnpnlOk.Size = new System.Drawing.Size(75, 23);
+            this.btnpnlOk.TabIndex = 5;
+            this.btnpnlOk.Text = "Ok";
+            this.btnpnlOk.UseVisualStyleBackColor = true;
+            this.btnpnlOk.Click += new System.EventHandler(this.btnpnlOk_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(3, 34);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(193, 20);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(3, 0);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(37, 13);
+            this.lblID.TabIndex = 3;
+            this.lblID.Text = "Kind 1";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lblID);
+            this.panel1.Controls.Add(this.btnpnlOk);
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Location = new System.Drawing.Point(12, 56);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 6;
+            this.panel1.Visible = false;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(147, 27);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(65, 23);
+            this.btnConfirm.TabIndex = 7;
+            this.btnConfirm.Text = "Ok";
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            this.btnConfirm.Click += new System.EventHandler(this.btnConfirm_Click);
+            // 
+            // txbaantalKinderen
+            // 
+            this.txbaantalKinderen.Location = new System.Drawing.Point(105, 29);
+            this.txbaantalKinderen.Name = "txbaantalKinderen";
+            this.txbaantalKinderen.Size = new System.Drawing.Size(36, 20);
+            this.txbaantalKinderen.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Aantal kinderen: ";
+            // 
+            // lblOut
+            // 
+            this.lblOut.AutoSize = true;
+            this.lblOut.Location = new System.Drawing.Point(12, 164);
+            this.lblOut.Name = "lblOut";
+            this.lblOut.Size = new System.Drawing.Size(136, 13);
+            this.lblOut.TabIndex = 10;
+            this.lblOut.Text = "Te betalen Ouder bijdrage: ";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.checkBox1.Size = new System.Drawing.Size(109, 17);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "?Een ouder gezin";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 185);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lblOut);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.txbaantalKinderen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Ouder bijdrage";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnpnlOk;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.TextBox txbaantalKinderen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblOut;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Ouderbijdrage/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ouderbijdrage
+{
+    public partial class Form1 : Form
+    {
+        //
+        // Vars
+        //
+        double jonger = 0;
+        double ouder = 0;
+        byte aantalKinderen;
+        byte opgeslagenKinderen;
+        bool eenOuderGezin;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //
+        //Methodes
+        //
+        private void Bereken()
+        {
+            double uit = 50;
+            if ((DateTime.Now.Year - dateTimePicker1.Value.Year) >= 10) { ouder += 1; }
+            else { jonger += 1; }
+            opgeslagenKinderen += 1;
+            if (opgeslagenKinderen >= aantalKinderen)
+            {
+                if(jonger > 3) { uit += 75; }
+                else { uit += (jonger * 25); }
+
+                if (ouder > 2) { uit += 62; }
+                else { uit += (ouder * 37); }
+
+                if (uit >= 150) uit = 150;
+
+                if (eenOuderGezin) uit = (uit / 100) * 75;
+                lblOut.Text = "Te betalen Ouder bijdrage: €" + uit.ToString();
+                jonger = 0;
+                ouder = 0;
+                aantalKinderen = 0;
+                opgeslagenKinderen = 0;
+                eenOuderGezin = false;
+            }
+        }
+
+        //
+        //Events
+        //
+
+        private void btnpnlOk_Click(object sender, EventArgs e)
+        {
+            Bereken();
+            lblID.Text = "Kind " + (opgeslagenKinderen + 1).ToString();
+            dateTimePicker1.Value = DateTime.Now;
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            aantalKinderen = byte.Parse(txbaantalKinderen.Text);
+            panel1.Visible = true;
+            eenOuderGezin = checkBox1.Checked;
+        }
+    }
+}
